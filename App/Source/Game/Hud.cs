053f8aaad4f481a658ef62d5018211404e042a32
ElBuenTocino <paumm8@gmail.com>
2025-05-20 19:23:23 +0200

    template rarillo what the hell

---
+++ App/Source/Game/Hud.cs
@@ -0,0 +1,52 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+
+namespace TcGame
+{
+  public class Hud : Actor
+  {
+        int persResc = 0, persCap = 0;
+        private Text txt;
+        public Hud() 
+        {
+            Layer = ELayer.Hud;
+            Font f = new Font("Data/Fonts/LuckiestGuy.ttf");
+            txt = new Text("", f);
+            txt.Position = new Vector2f(10, 10);
+            txt.FillColor = new Color(Color.Yellow);
+            txt.DisplayedString = ($"Persones Rescatades: {persResc}\nPersones Capturades: {persCap}");
+        }
+
+        public override void Update(float dt)
+        {
+            Console.Clear();
+
+            Console.WriteLine(Position);
+            Console.WriteLine(txt);
+
+            base.Update(dt);
+            SetText();
+        }
+
+        public void SetText()
+        {
+            txt.DisplayedString = ($"Persones Rescatades: {persResc}\nPersones Capturades: {persCap}");
+        }
+
+        public void IncreaseRescued()
+        {
+            persResc++;
+        }
+
+        public void IncreaseCaptured()
+        {
+            persCap++;
+        }
+        public override void Draw(RenderTarget target, RenderStates states)
+        {
+            target.Draw(txt);
+        }
+    }
+}
+
---
+++ App/Source/Game/Plane.cs
@@ -0,0 +1,30 @@
+﻿using SFML.Graphics;
+using System.Collections.Generic;
+
+namespace TcGame
+{
+    public class Plane : AnimatedActor
+    {
+
+
+        public override void Draw(RenderTarget target, RenderStates states)
+        {
+            base.Draw(target, states);
+        }
+
+        private void CheckCollision()
+        {
+            List<Person> lPerson = Engine.Get.Scene.GetAll<Person>();
+
+            foreach (Person person in lPerson) 
+            {
+                if (person.GetGlobalBounds().Intersects(this.GetGlobalBounds()))
+                {
+                    Engine.Get.Scene.Destroy(person);
+                    Hud hud = Engine.Get.Scene.GetFirst<Hud>();
+                    hud.IncreaseRescued();
+                }
+            }
+        }
+    }
+}
